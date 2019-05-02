8e37f6146d179734d7bf2e953c17956642751d0d
Mille737 <mille737@gmail.com>
2019-05-02 09:52:42 +0200

    svømmer type motionist/konkurrence

--- Delfin/src/Svømmer.java
+++ Delfin/src/Svømmer.java
@@ -3,13 +3,16 @@ public class Svømmer {
     public String name;
     public int age;
     public boolean activeMember;
+    public String membershipType;
 
     public void setInfo() {
         name = "Camille";
         age = 23;
         activeMember = true;
+        membershipType = "Motionist";
         System.out.println("Svømmer navn: " + name);
         System.out.println("Svømmer alder: " + age);
+        System.out.println("Svømmer type: " + membershipType);
         System.out.println("Svømmer aktivt: " + activeMember);
     }
 }
