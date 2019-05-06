8ff1f0bfc36d4c1c4b9ece128a42bf8c01c5b49d
Robin Zernickov <robinzernickov@hotmail.com>
2019-05-06 02:37:46 +0200

    viewSwimmer() in progress

--- Delfin/src/FileIn.java
+++ Delfin/src/FileIn.java
@@ -1,6 +1,8 @@
 import java.io.*;
+import java.lang.reflect.Array;
 import java.lang.reflect.Member;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class FileIn
@@ -17,25 +19,25 @@ public class FileIn
             BufferedWriter bw = new BufferedWriter(fw);
 
             System.out.println("Navn:");
-            String name = sc.nextLine();
+            String name = sc.next();
 
             System.out.println("Alder:");
-            int age = sc.nextInt();
+            String age = sc.next();
 
             System.out.println("Aktivitets Status:");
-            boolean aktivitetsStatus = sc.nextBoolean();
+            String aktivitetsStatus = sc.next();
 
             System.out.println("Engagement:");
             String engagement = sc.next();
 
             System.out.println("CPR-Nummer:");
-            int cpr = sc.nextInt();
+            String cpr = sc.next();
 
             System.out.println("Pris:");
-            int price = sc.nextInt();
+            String price = sc.next();
 
             System.out.println("Er det betalt?");
-            boolean isPaid = sc.nextBoolean();
+            String isPaid = sc.next();
 
             bw.write(name + "\t" + age + "\t" + aktivitetsStatus + "\t" + engagement + "\t" + cpr + "\t" + price + "\t" + isPaid + "\n");
 
@@ -53,22 +55,34 @@ public class FileIn
 
     }
 
-    public void viewSwimmer(ArrayList<Members>members)
+    public void viewSwimmer()
     {
-        Members member = new Members();
+        try {
+            List<Members> members = new ArrayList<>();
+            BufferedReader reader = new BufferedReader(new FileReader(filepath));
+            String line;
 
-        System.out.println("Søg: ");
-        String søgning;
-        søgning = sc.next();
 
-        for (int i=0; i<members.size(); i++)
-        {
-            if(søgning.equalsIgnoreCase(member.getName()))
-            {
-                System.out.println(member.getAge());
+            while ((line = reader.readLine()) != null) {
+                String[] data = line.split("\t");
+                Members m = new Members(data[0], data[1], data[2], data[3], data[4], data[5], data[6]);
+                members.add(m);
             }
-        }
+            reader.close();
 
+            for(Members member: members) {
+                System.out.println(member);
+            }
+
+            String søgning = sc.next();
+
+            for(Members member: members){
+                if(member.getName().equalsIgnoreCase(søgning)){
+                    System.out.println(member);
+                }
+            }
+
+        } catch(Exception e){System.out.println(e);}
 
 
     }
